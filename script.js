ea17065a0cc97850ab0c089deea15cc74d877a41
yasserlakrid <yasserlakrid36@gmail.com>
2026-01-09 18:48:23 +0100

    'styling'

--- script.js
+++ script.js
@@ -10,41 +10,38 @@ function createbtns() {
     btns.innerHTML += `
     <button class='number _${i}' id='number'>${i}</button>
     `;
-    if (i == 3 || i == 6 || i == 9) {
-      btns.innerHTML += "<br>";
-    }
   }
   btns.innerHTML += `
   
-  <button id='ope' >+</button>
-  <button id='ope'>-</button>
-  <button id='ope'>*</button>
-  <button id='ope'>/</button>
+  <button class ='ope' style='grid-row : 1/2' id='ope' >+</button>
+  <button class ='ope' style='grid-row : 2/3' id='ope'>-</button>
+  <button class ='ope' style='grid-row : 3/4'id='ope'>*</button>
+  <button class ='ope' style='grid-row : 4/5' id='ope'>/</button>
   
   
   <button id='equ'>=</button>
-
+  <button class='number _0' id='number'>0</button>
   `;
 }
 createbtns();
 function createdisplay() {
   for (i = 0; i < btns.children.length; i++) {
     btns.children[i].addEventListener("click", (e) => {
-      console.log('the number id is ',num.id,' the event is ',e.target.id )
       if (e.target.id !== "equ") {
-        if ((e.target.id == "number" && num.id == "number") || (num.value !=="" && (e.target.id !== num.id )) ) {
+        if (
+          (e.target.id == "number" && num.id == "number") ||
+          (num.value !== "" && e.target.id !== num.id)
+        ) {
           num.value += e.target.innerHTML;
           num.id = e.target.id;
-          display.innerHTML = num.value;
+          display.innerText = num.value;
         }
-
-
       }
 
       if (e.target.id == "equ") {
         display.innerHTML += "=" + eval(num.value + "*1");
         num.value = "";
-        num.id = 'number';
+        num.id = "number";
       }
     });
   }
